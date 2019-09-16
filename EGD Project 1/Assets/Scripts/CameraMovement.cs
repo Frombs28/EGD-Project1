8e45ddc97e5c40cabe3ca9021547510d6aebd624
sophierichards <richas3@rpi.edu>
2019-09-16 19:34:48 -0400

    Basic turns and camera movement

---
+++ EGD Project 1/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [Range(0,1)]
+    [SerializeField] float speed = 0.1f;
+    Vector3 offset;
+    //target should be set to the first player to get correctOffset
+    public GameObject target = null;
+    void Start()
+    {
+        if(target!=null){
+            offset = transform.position - target.transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TrackTarget();
+    }
+    public void SetTarget(GameObject newTarget){
+        target = newTarget;
+    }
+    public void TrackTarget(){
+        //camera code to track target here
+        //change this to better code later..........
+        if(target==null){
+            return;
+        }
+        transform.position = Vector3.Slerp(transform.position,target.transform.position + offset,speed);
+    }
+}
---
+++ EGD Project 1/Assets/Scripts/TurnManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour
+{
+    int currentPlayer = 1;
+    public GameObject player1 = null;
+    public GameObject player2 = null;
+    int numTaken = 0;
+    public int maxTurns = 5; 
+    CameraMovement cameraMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraMovement = FindObjectOfType<CameraMovement>();
+        cameraMovement.SetTarget(player1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SwitchTurns(){
+        //TO DO: fade screen
+        if(currentPlayer==1){
+            currentPlayer = 2;
+            cameraMovement.SetTarget(player2);
+        }
+        else{
+            currentPlayer = 1;
+            cameraMovement.SetTarget(player1);
+        }
+        numTaken = 0;
+    }
+    public void TakeTurn(){
+        numTaken++;
+        if(numTaken==maxTurns){
+            SwitchTurns();
+        }
+    }
+    public void SkipTurn(){
+        //TO DO: reveal the info here
+        SwitchTurns();
+    }
+}
